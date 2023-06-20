86777ad173928b44ca1a29970be14070fdf8afaf
fbuenano <fbuenano@fiec.espol.edu.ec>
2023-06-20 09:16:39 -0500

    v4
    v4

--- carshop-react/django-env/carshop/car/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from datetime import datetime
-from car.models import  Clientes
-
-class ClientesForm(forms.ModelForm):
-    class Meta:
-        model = Clientes
-        fields = '__all__'
